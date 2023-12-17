9afea47a8527f723c9f7b5c56f3ca8401a01f37b
MariemBaccari <mariembaccari70@gmail.com>
2023-12-17 15:13:46 +0000

    Fix start script

--- 109-Load/latency_socialNetwork.py
+++ 109-Load/latency_socialNetwork.py
@@ -66,8 +66,8 @@ def run_load():
     return start_time, end_time
 
 def run_load_qos():
-    commandToRunLoad1 = "../wrk2/wrk -D exp -t 6 -c 200 -d 120s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.90.36.43:8080/wrk2-api/post/compose -R 1000"
-    commandToRunLoad2 = "../wrk2/wrk -D exp -t 6 -c 200 -d 30s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.90.36.43:8080/wrk2-api/post/compose -R 1000"
+    commandToRunLoad1 = "../wrk2/wrk -D exp -t 6 -c 200 -d 300s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.90.36.43:8080/wrk2-api/post/compose -R 1000"
+    commandToRunLoad2 = "../wrk2/wrk -D exp -t 6 -c 200 -d 60s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.90.36.43:8080/wrk2-api/post/compose -R 1000"
 
     print("Start running load")
 
@@ -150,7 +150,7 @@ def main():
     #    print('STDERR:', stderr.decode())
     #    exit(1)  # Exit the script with an error code
 
-    start_time, end_time = run_load()
+    start_time, end_time = run_load_qos()
 
     traces = fetch_traces(jaeger_url, service_name, start_time, end_time)
 
