649cfe0db2f0be6d14539997da0b92145f444e3c
sgueissa <stuart.gueissaz@epfl.ch>
2023-12-18 13:31:47 +0100

    Modify load sn

--- 109-Load/latency_socialNetwork.py
+++ 109-Load/latency_socialNetwork.py
@@ -69,7 +69,7 @@ def run_load():
 
 def run_load_qos():
     commandToRunLoad1 = "../wrk2/wrk -D exp -t 1 -c 32 -d 300s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.89.3.7:8080/wrk2-api/post/compose -R 5000"
-    commandToRunLoad2 = "../wrk2/wrk -D exp -t 1 -c 32 -d 30s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.89.3.7:8080/wrk2-api/post/compose -R 5000"
+    commandToRunLoad2 = "../wrk2/wrk -D exp -t 1 -c 32 -d 45s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.89.3.7:8080/wrk2-api/post/compose -R 5000"
     #commandToRunLoad1 = "../wrk2/wrk -D exp -t 1 -c 4 -d 300s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.90.36.43:8080/wrk2-api/post/compose -R 500"
     #commandToRunLoad2 = "../wrk2/wrk -D exp -t 1 -c 4 -d 60s -L -p -s ./wrk2/scripts/social-network/compose-post.lua http://10.90.36.43:8080/wrk2-api/post/compose -R 500"
 
