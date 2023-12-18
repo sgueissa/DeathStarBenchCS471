2c148980912989393fc199affb0612eba423bf85
sgueissa <stuart.gueissaz@epfl.ch>
2023-12-19 00:49:45 +0100

    test

--- 109-Load/latency_mediaService.py
+++ 109-Load/latency_mediaService.py
@@ -67,7 +67,7 @@ def run_load():
     return start_time, end_time
 
 def run_load_qos():
-    commandToRunLoad1 = "../wrk2/wrk -D exp -t 1 -c 16 -d 300s -L -s ./wrk2/scripts/media-microservices/compose-review.lua http://10.89.3.7:8080/wrk2-api/review/compose -R 3000"
+    commandToRunLoad1 = "../wrk2/wrk -D exp -t 1 -c 16 -d 300s -L -p -s ./wrk2/scripts/media-microservices/compose-review.lua http://10.89.3.7:8080/wrk2-api/review/compose -R 3000"
     commandToRunLoad2 = "../wrk2/wrk -D exp -t 1 -c 16 -d 30s -L -s ./wrk2/scripts/media-microservices/compose-review.lua http://10.89.3.7:8080/wrk2-api/review/compose -R 6000"
     #commandToRunLoad1 = "../wrk2/wrk -D exp -t 1 -c 4 -d 300s -L -s ./wrk2/scripts/media-microservices/compose-review.lua http://10.90.36.43:8080/wrk2-api/review/compose -R 500"
     #commandToRunLoad2 = "../wrk2/wrk -D exp -t 1 -c 4 -d 60s -L -s ./wrk2/scripts/media-microservices/compose-review.lua http://10.90.36.43:8080/wrk2-api/review/compose -R 500"
